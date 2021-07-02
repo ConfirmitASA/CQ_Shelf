f71730a67ca8c536724f69606e496df1f81d7701
ElenaV <helenavlasova39@gmail.com>
2021-07-02 13:13:39 +0300

    Added shelf render to main render

--- runtime/component.js
+++ runtime/component.js
@@ -1,9 +1,7 @@
 export default (question, customQuestionSettings, questionViewSettings) => {
   // TODO: put your code here
   // client api docs: https://confirmit.github.io/ResponsiveSurveyRenderingV2/index.html
-  const container = document.getElementById(question.id);
-  const div = document.createElement('div');
-  div.innerText = 'hello world';
-  container.appendChild(div);
+  const shelf = new customQuestionsLibrary.Shelf(question, customQuestionSettings);
+  shelf.render();
 
 };
